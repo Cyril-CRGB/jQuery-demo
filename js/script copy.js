d5caaf13df05204d4fd941c0889752ec92f78168
Cyril-CRGB <>
2023-09-04 06:07:15 +0000

    chaining methods

--- js/script copy.js
+++ js/script copy.js
@@ -28,18 +28,11 @@ $(document).ready(function () {
         $(".stream1").fadeTo(1000, 0.5); //with two arguments, speed and opacity (between 0 and 1)
     });
 
-    $("#stream2_btn").on("click", function () {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-    });
-    $("#stream3_btn").on("click", function () {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-    });
+    $("#myButton").removeClass("blueBox").addClass("border");
+    $("p").css("color", "blue").slideUp(2000).slideDown(2000);
+    $("a").attr("href", "http://www.example.com");
+
+
 
 
 }); 
